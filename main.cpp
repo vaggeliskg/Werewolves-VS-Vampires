074a4cc72da28ce11a9ebc44de3ea5b1c885ac3a
vaggelis kalabokis <vaggeliskg@gmail.com>
2022-11-30 17:16:17 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include "state.h"
+
 using namespace std;
+
+void board(int x, int y) {
+
+	Map map;
+	
+	//map.set_lenght(x);
+	//map.set_width(y);
+
+
+}
+
+
+
+
 
 
 int main() {
-	cout << "hi";
+	int x, y;
+	cout << "Welcome to Werewolves vs Vampires\n";
+	cout << "Please provide the length of the map: ";
+	cin >> x;
+	cout << "\n";
+	cout << "Please provide the width of the map: ";
+	cin >> y;
+	cout << "\n";
+
+	board(x, y);
+
+
 }
